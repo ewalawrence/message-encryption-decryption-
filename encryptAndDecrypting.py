859520d8286f20fa6633789903ee425ef28a34d5
Lawrence Ewa <ewalawrence1991@gmail.com>
2024-09-15 14:19:44 +0100

    added the break option

--- encryptAndDecrypting.py
+++ encryptAndDecrypting.py
@@ -10,7 +10,7 @@ keys = encryptionCharacters.copy()
 random.shuffle(keys)
 
 while True:
-    options = print("Chose what to do, encrypt or decrypt(E or D): ")
+    options = input("Chose what to do, encrypt or decrypt( e or d ), or q to quit: ").lower()
     if options == "e":
         #ENCRYPT
         message = input("Enter your texts for encryption: ")
@@ -19,7 +19,7 @@ while True:
             index = encryptionCharacters.index(letter)
             cipherText += keys[index]
         print(f"Original message: {message}")
-        print(f"Encrypted message: {cipherText}")
+        print(f"Encrypted message: {cipherText} 🔒")
 
     elif options == "d":
         #DECRYPT
@@ -28,8 +28,12 @@ while True:
         for letter in cipherText:
             index = keys.index(letter)
             message += encryptionCharacters[index]
-        print(f"Encrypted message: {cipherText}")
+        print(f"Encrypted message: {cipherText} 🔒")
         print(f"Translated message: {message}")
 
-else:
-    print("Select a choice, encrypt or decrypt(e or d): ")
+    elif options == "q":
+        print("Program Closed🛄.")
+        break
+
+    else:
+        print("Select a choice, encrypt or decrypt(e or d): ")
